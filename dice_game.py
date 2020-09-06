0889a3e9f45b15931def16fee0c24dee3ec59c51
“jy8474np” <jy8474np@go.minnstate.edu>
2020-09-06 17:43:42 -0500

    Player and opponent dice, rolls, and displays functional and commented.

--- dice_game.py
+++ dice_game.py
@@ -1,2 +1,51 @@
 
-print('Hello, Dice Game!')
+# import random to generate random dice rolls
+import random
+
+# import time to add player suspense and improve game play flow
+# Delay inspiration/reference: https://opensource.com/article/17/10/python-101
+import time
+
+# create two dice for the player, with each die comprised of values from one to six
+player_die01 = random.randint(1,6)
+player_die02 = random.randint(1,6)
+
+# Calculate the sum of die01 and die02 to determine the player's roll total
+player_total_roll = player_die01 + player_die02
+
+# create two dice for the opponent, with each die comprised of values from one to six
+opponent_die01 = random.randint(1,6)
+opponent_die02 = random.randint(1,6)
+
+# Calculate the sum of die01 and die02 to determine the opponents's roll total
+opponent_total_roll = opponent_die01 + opponent_die02
+
+# Display results of player's turn.
+# Add time delays for player suspense and game play flow
+
+print("You rolled:")
+time.sleep(1)
+print(player_die01)
+time.sleep(1)
+print ("... and ...")
+time.sleep(1)
+print(player_die02)
+time.sleep(1)
+print("... for a total of ...")
+time.sleep(2)
+print(str(player_total_roll))
+
+# Display results of opponent's turn.
+# Add time delays for player suspense and game play flow
+
+print("Your opponent rolled:")
+time.sleep(1)
+print(opponent_die01)
+time.sleep(1)
+print ("... and ...")
+time.sleep(1)
+print(opponent_die02)
+time.sleep(1)
+print("... for a total of ...")
+time.sleep(2)
+print(str(opponent_total_roll))
